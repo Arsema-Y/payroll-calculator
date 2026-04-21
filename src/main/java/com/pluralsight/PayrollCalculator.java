1649601bc01e77f02889ba50fca99597729f3228
mili Arsema <meleymm88@gmail.com>
2026-04-21 03:01:00 -0500

    Modified code to incorperate methods

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -7,17 +7,46 @@ public class PayrollCalculator {
         //Build an input system: keyboard
         Scanner keyboard = new Scanner(System.in);
 
-        //gather info using input system
-        System.out.println("What's your name? ");
-        String name = keyboard.nextLine();
+      // call methods
+        inputName(keyboard);
 
-        System.out.println("How many hours did you work? ");
-        float hoursWorked = keyboard.nextFloat();
+        double totalPay = getTotalPay(keyboard); //getHoursWorked + getPayRate methods called inside getTotalPay
+                                                 //because they're nessecary for calculating the total pay
 
-        System.out.println("What's your hourly pay rate? ");
-        float payRate = keyboard.nextFloat();
 
-        //calculate pay: regular hours + overtime
+        //display payroll
+        System.out.println("Your total pay is $" + totalPay);
+    }
+
+  //smaller methods practice
+    //gather info using input system
+    //get name
+    public static void inputName(Scanner input){
+        System.out.print("What's your name? ");
+        String name = input.nextLine();
+    }
+
+    //get total hours
+    public static float getHoursWorked(Scanner input){
+        System.out.print("How many hours did you work? ");
+        float hoursWorked = input.nextFloat();
+        return hoursWorked;
+    }
+
+    //get pay rate
+    public static float getPayRate(Scanner input){
+        System.out.print("What's your hourly pay rate? ");
+        float payRate = input.nextFloat();
+
+        return payRate;
+    }
+
+    //calculate pay
+    public static double getTotalPay(Scanner input){
+        float hoursWorked = getHoursWorked(input);
+        float payRate = getPayRate(input);
+
+        //regular hours + overtime
         double regularPay = 40 * payRate;
 
         double overtimePay = regularPay;
@@ -28,8 +57,7 @@ public class PayrollCalculator {
              */
             overtimePay = ((hoursWorked - 40) * (1.5 * payRate)) + regularPay;
         }
-
-        //display payroll
-        System.out.println("Your total pay is $" + overtimePay);
+        return overtimePay;
     }
+
 }
